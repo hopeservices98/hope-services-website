66a35b37611204d35d84287bfa0dd2d0773dfb9a
hopeservices98 <rak.angelo98@gmail.com>
2025-08-31 13:50:20 +0300

    Commit initial

--- index.tsx
+++ index.tsx
@@ -7,6 +7,7 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
+// FIX: 'ReactDOM' and 'React' were used as UMD globals. Imported them to work in a module context.
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
